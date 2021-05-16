a36980e8b4de4e9cae60b8470a85ccc37dde9381
t-tsutsui <t.tsutsui9117@gmail.com>
2021-05-16 23:52:00 +0900

    execute.batにJAVA_HOME PATH指定を追加

--- dist/DBDUMP/execute.bat
+++ dist/DBDUMP/execute.bat
@@ -12,8 +12,12 @@ set date_str=%date:~-10,4%%date:~-5,2%%date:~-2,2%
 rem 現在時刻（HHMMSS）の取得
 set time_str=%time: =0%
 set time_str=%time_str:~0,2%%time_str:~3,2%%time_str:~6,2%
+
 rem 現在ディレクトリの取得
-set current_dir=%~dp0
+rem set PATH=[jdk11 directory you downloaded];%PATH%
+
+rem JAVA_HOME設定
+set path
 
 java -cp %current_dir%resources\prop;.\resources\lib\dbdump-0.0.1-SNAPSHOT-all.jar ^
   org.springframework.batch.core.launch.support.CommandLineJobRunner ^
